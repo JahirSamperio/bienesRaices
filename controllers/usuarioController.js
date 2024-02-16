bee7a317878d0583f4dd7ea987eb112b762f82e7
Jahir Alberto Samperio <jahirsampe1234@gmail.com>
2024-02-15 22:19:04 -0600

    Enrutamiento y formulario de inicio de sesion finalizado

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,10 +1,23 @@
 const formularioLogin = (req, res) => {
     res.render('auth/login',{
-        autenticado: false
+        pagina: "Iniciar sesion"
     })
 }
 
+const formularioRegistro = (req, res) => {
+    res.render('auth/registro',{
+        pagina: "Crear cuenta"
+    })
+}
+
+const formularioOlvidePassword = (req, res) => {
+    res.render('auth/olvide-password',{
+        pagina: "Recupera tu acceso a Bienes Raices"
+    })
+}
 //Export es multiple y export default solo es uno por archivo
 export {
-    formularioLogin
+    formularioLogin,
+    formularioRegistro,
+    formularioOlvidePassword
 }
